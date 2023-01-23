16204b5d396fc50b78734b5794f05fe6385dd3ea
rbeauclair <rbs@impa.br>
2023-01-23 17:04:53 -0300

    conteúdo usando lista

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
+dados = {
+    1: {"nome":"Nebulosa de Carina",
+        "legenda":"webbtelescope.org / NASA / James Webb"},
+    2: {"nome":"Galáxia NGC 1079",
+        "legenda":"nasa.org / NASA / Hubble"}
+}
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1: {"nome":"Nebulosa de Carina",
+            "legenda":"webbtelescope.org / NASA / James Webb"},
+        2: {"nome":"Galáxia NGC 1079",
+            "legenda":"nasa.org / NASA / Hubble"}
+    }
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
