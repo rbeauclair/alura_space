468474428be29e471726b4406c3dd25688043326
rbeauclair <rbs@impa.br>
2023-01-24 16:29:56 -0300

    Inclusão do banco de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,20 +1,10 @@
-from django.shortcuts import render
-
-dados = {
-    1: {"nome":"Nebulosa de Carina",
-        "legenda":"webbtelescope.org / NASA / James Webb"},
-    2: {"nome":"Galáxia NGC 1079",
-        "legenda":"nasa.org / NASA / Hubble"}
-}
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
 
 def index(request):
-    dados = {
-        1: {"nome":"Nebulosa de Carina",
-            "legenda":"webbtelescope.org / NASA / James Webb"},
-        2: {"nome":"Galáxia NGC 1079",
-            "legenda":"nasa.org / NASA / Hubble"}
-    }
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia":fotografia})
