abb8471c5e7af0762a50c9737d476b908e606883
rbeauclair <rbs@impa.br>
2023-01-26 13:46:33 -0300

    Inclusão de categorias

--- galeria/models.py
+++ galeria/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class Fotografia(models.Model):
+
+    CATEGORIAS = [
+        ("NEBULOSA", "Nebulosa"), 
+        ("ESTRELA", "Estrela"),
+        ("GALÁXIA", "Galáxia"),
+        ("PLANETA", "Planeta"),
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(choices=CATEGORIAS, max_length=100, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
