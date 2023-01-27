2d7682ef4ff4c85f07a7d3109b114fb81b1bc64e
rbeauclair <rbs@impa.br>
2023-01-27 11:09:09 -0300

    Template de medias

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from galeria.views import index, imagem
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
